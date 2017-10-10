0b8740fe5ed63d28b620d0afe8a553a2f04eb3bf
Kristen Norris <krnorris65@gmail.com>
2017-10-10 15:26:57 -0500

    modified code to the for loop to reference the key values in coinValues

--- cashToCoins.js
+++ cashToCoins.js
@@ -17,20 +17,20 @@ for(let key in coinValues) {
     let coinTotal = 0;
 
     if(key === "quarters") {
-        coinTotal = Math.floor(remainingAmount / .25);
-        remainingAmount -= coinTotal * .25;
+        coinTotal = Math.floor(remainingAmount / coinValues.quarters);
+        remainingAmount -= coinTotal * coinValues.quarters;
         piggyBank[key] = coinTotal;
     } else if (key === "dimes") {
-        coinTotal = Math.floor(remainingAmount / .10);
-        remainingAmount -= coinTotal * .10;
+        coinTotal = Math.floor(remainingAmount / coinValues.dimes);
+        remainingAmount -= coinTotal * coinValues.dimes;
         piggyBank[key] = coinTotal;
     } else if (key === "nickles") {
-        coinTotal = Math.floor(remainingAmount / .05);
-        remainingAmount -= coinTotal * .05;
+        coinTotal = Math.floor(remainingAmount / coinValues.nickles);
+        remainingAmount -= coinTotal * coinValues.nickles;
         piggyBank[key] = coinTotal;
     } else if (key === "pennies") {
-        coinTotal = Math.floor(remainingAmount / .01);
-        remainingAmount -= coinTotal * .01;
+        coinTotal = Math.floor(remainingAmount / coinValues.pennies);
+        remainingAmount -= coinTotal * coinValues.pennies;
         piggyBank[key] = coinTotal;
     }
 
